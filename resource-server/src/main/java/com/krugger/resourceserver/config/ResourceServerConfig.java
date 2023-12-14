b44c1ac0776e6f910a174e6a4f2176c0f0f84c6b
Ronald Alvarado <ronald.alvarado@epn.edu.ec>
2023-12-14 10:45:23 -0500

    Adsd resource server

--- resource-server/src/main/java/com/krugger/resourceserver/config/ResourceServerConfig.java
+++ resource-server/src/main/java/com/krugger/resourceserver/config/ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package com.krugger.resourceserver.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,12 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +30,15 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
+                .cors(cors -> {
+                    cors.configurationSource(request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:8080", "*"));
+                        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                        config.setAllowCredentials(true);
+                        return config;
+                    });
+                })
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/empleados").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
                         .requestMatchers(HttpMethod.PUT, "/api/empleados").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
